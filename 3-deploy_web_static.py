b836185a2cba47fb1b4b901bf2c6321fe03ae4be
Milla2000 <millajesso2000@gmail.com>
2024-07-17 01:25:59 +0300

    added path to ssh key

--- 3-deploy_web_static.py
+++ 3-deploy_web_static.py
@@ -7,7 +7,9 @@ from fabric.api import local
 from fabric.api import put
 from fabric.api import run
 
-env.hosts = ["54.160.85.72", "35.175.132.106"]
+env.hosts = ["54.234.89.183", "100.26.165.1"]
+env.user = "ubuntu"  
+env.key_filename = "/home/milla/.ssh/id_rsa"
 
 
 def do_pack():
